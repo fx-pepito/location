a19594e8b115413e298f6af3cc8c7612dc033502
fx-pepito <117949857+fx-pepito@users.noreply.github.com>
2022-11-10 22:49:50 +0100

    Ajout V1

---
+++ location v1 crypted/fxmanifest.lua
@@ -0,0 +1,16 @@
+-- Version
+fx_version 'cerulean'
+game 'gta5'
+type 'standalone'
+version '1.0.0'
+lua54 'yes'
+
+client_scripts {
+    '@menuv/menuv.lua',
+    'client/*.lua'
+}
+
+shared_scripts {
+    'settings.lua'
+}
+dependency '/assetpacks'
